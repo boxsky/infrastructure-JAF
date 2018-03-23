4eabbe7a9e2aba9c764657b41e10d87803e12948
leo.ma <leo.ma@aihuishou.com>
2018-03-23 21:39:41 +0800

    change frame demo database

--- config/database.php
+++ config/database.php
@@ -1,6 +1,6 @@
 <?php
 $config['xhj_dev'] = array (
-    'dsn'=>'mysql:host=192.168.10.119;dbname=airent_new_2017',
+    'dsn'=>'mysql:host=192.168.1.119;dbname=airent_new_2017',
     'username' => 'root',
     'password' => 'root',
     'init_statements' => ['SET CHARACTER SET utf8', 'SET NAMES utf8'],
