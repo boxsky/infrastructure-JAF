a2657d1c3482e1dea83c4660ae6d2471e0954e59
hex <1546151406@qq.com>
2019-03-15 11:41:33 +0800

    transfer test ip change

--- config/database.php
+++ config/database.php
@@ -1,6 +1,6 @@
 <?php
 $config['xhj_dev'] = array (
-    'dsn'=>'mysql:host=118.31.223.114;dbname=airent_new_2017',
+    'dsn'=>'mysql:host=47.110.141.101;dbname=airent_new_2017',
     'username' => 'root',
     'password' => 'X1am9hVAnj1',
     'init_statements' => ['SET CHARACTER SET utf8', 'SET NAMES utf8'],
